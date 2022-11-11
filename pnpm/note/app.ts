3b663368b599b72bdd2591e382fec1b0dfb57b59
Sungwook Choi <sungwook.csw@gmail.com>
2022-11-11 23:42:19 +0900

    feat: install lodash and print add

--- pnpm/note/app.ts
+++ pnpm/note/app.ts
@@ -1,5 +1,7 @@
 import * as dayjs from 'dayjs'
 import * as moment from "moment"
+import * as lodash from "lodash"
 
 console.log(dayjs().format());
 console.log(moment().format());
+console.log(lodash.add(1, 2));
