02a3d399b5a9fb04e91d76fd21155bdf76d91dcb
Sungwook Choi <sungwook.csw@gmail.com>
2022-11-14 23:13:40 +0900

    feat: make simple counter

--- pnpm/note/script.ts
+++ pnpm/note/script.ts
@@ -1,6 +1,5 @@
 const button: HTMLElement = document.getElementById("hello")
-const buttonOnClickHandler = (): void => {
-    alert("hello");
-}
+const span: HTMLElement = document.getElementById("cnt")
 
+const buttonOnClickHandler = (): void => { span.innerText = String(parseInt(span.innerText) + 1); }
 button.addEventListener("click", buttonOnClickHandler);
