76d0418579f607be4afdbbae25c0aaf778258e13
Sungwook Choi <sungwook.csw@gmail.com>
2022-11-15 23:55:58 +0900

    feat: add function keyword

--- pnpm/note/script.ts
+++ pnpm/note/script.ts
@@ -1,5 +1,5 @@
 const button: HTMLElement = document.getElementById("hello")
 const span: HTMLElement = document.getElementById("cnt")
 
-const buttonOnClickHandler = (): void => { span.innerText = String(parseInt(span.innerText) + 1); }
+const buttonOnClickHandler: function = (): void => { span.innerText = String(parseInt(span.innerText) + 1); }
 button.addEventListener("click", buttonOnClickHandler);
